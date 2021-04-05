4745ade65936b879bf85154c4eac5f584412971f
pong3ds <chaiyapong@3dsinteractive.com>
2021-04-05 15:20:46 +0700

    PAM servers diagram

--- pam-servers.py
+++ pam-servers.py
@@ -1,7 +1,34 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
+from diagrams import Cluster, Diagram
+from diagrams.aws.compute import ECS, EKS, Lambda
 from diagrams.aws.network import ELB
+from diagrams.aws.database import ElastiCache, RDS, ElasticacheForRedis
+from diagrams.aws.integration import SQS
+from diagrams.aws.storage import S3
+from diagrams.aws.analytics import ManagedStreamingForKafka
+from diagrams.aws.general import GenericFirewall
 
-with Diagram("pam servers", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+with Diagram("PAM Servers", show=False):
+    lb = ELB("lb")
+
+    with Cluster("EKS"):
+        with Cluster("Workers"):
+            workers = [ECS("worker1"),
+                       ECS("worker2"),
+                       ECS("worker3")]
+
+        fw = GenericFirewall("Firewall")
+
+        with Cluster("Databases"):
+            dbs = [ECS("db1"),
+                   ECS("db2"),
+                   ECS("db3")]
+
+    s3 = S3("Images and Files")
+    kfk = ManagedStreamingForKafka("Kafka Streams")
+    redis = ElasticacheForRedis("Redis")
+
+    lb >> workers
+    workers >> fw >> dbs
+    workers >> s3
+    workers >> kfk
+    workers >> redis

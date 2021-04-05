a2fb846a8a3b172a92d982261767f08d37e7cde6
pong3ds <chaiyapong@3dsinteractive.com>
2021-04-05 15:29:43 +0700

    Update packages

--- pam-servers.py
+++ pam-servers.py
@@ -7,7 +7,111 @@ from diagrams.aws.storage import S3
 from diagrams.aws.analytics import ManagedStreamingForKafka
 from diagrams.aws.general import GenericFirewall
 
-with Diagram("PAM Servers", show=False):
+with Diagram("PAM Servers Mini", show=False):
+    lb = ELB("lb")
+
+    with Cluster("EKS"):
+        with Cluster("Workers"):
+            workers = [ECS("worker1"),
+                       ECS("worker2"),
+                       ECS("worker3")]
+
+        fw = GenericFirewall("Firewall")
+
+        with Cluster("Databases"):
+            dbs = [ECS("db1"),
+                   ECS("db2"),
+                   ECS("db3")]
+
+    s3 = S3("Images and Files")
+    kfk = ManagedStreamingForKafka("Kafka Streams")
+    redis = ElasticacheForRedis("Redis")
+
+    lb >> workers
+    workers >> fw >> dbs
+    workers >> s3
+    workers >> kfk
+    workers >> redis
+
+with Diagram("PAM Servers Basic", show=False):
+    lb = ELB("lb")
+
+    with Cluster("EKS"):
+        with Cluster("Workers"):
+            workers = [ECS("worker1"),
+                       ECS("worker2"),
+                       ECS("worker3")]
+
+        fw = GenericFirewall("Firewall")
+
+        with Cluster("Databases"):
+            dbs = [ECS("db1"),
+                   ECS("db2"),
+                   ECS("db3")]
+
+    s3 = S3("Images and Files")
+    kfk = ManagedStreamingForKafka("Kafka Streams")
+    redis = ElasticacheForRedis("Redis")
+
+    lb >> workers
+    workers >> fw >> dbs
+    workers >> s3
+    workers >> kfk
+    workers >> redis
+
+with Diagram("PAM Servers Pro", show=False):
+    lb = ELB("lb")
+
+    with Cluster("EKS"):
+        with Cluster("Workers"):
+            workers = [ECS("worker1"),
+                       ECS("worker2"),
+                       ECS("worker3")]
+
+        fw = GenericFirewall("Firewall")
+
+        with Cluster("Databases"):
+            dbs = [ECS("db1"),
+                   ECS("db2"),
+                   ECS("db3")]
+
+    s3 = S3("Images and Files")
+    kfk = ManagedStreamingForKafka("Kafka Streams")
+    redis = ElasticacheForRedis("Redis")
+
+    lb >> workers
+    workers >> fw >> dbs
+    workers >> s3
+    workers >> kfk
+    workers >> redis
+
+with Diagram("PAM Servers Pro XL", show=False):
+    lb = ELB("lb")
+
+    with Cluster("EKS"):
+        with Cluster("Workers"):
+            workers = [ECS("worker1"),
+                       ECS("worker2"),
+                       ECS("worker3")]
+
+        fw = GenericFirewall("Firewall")
+
+        with Cluster("Databases"):
+            dbs = [ECS("db1"),
+                   ECS("db2"),
+                   ECS("db3")]
+
+    s3 = S3("Images and Files")
+    kfk = ManagedStreamingForKafka("Kafka Streams")
+    redis = ElasticacheForRedis("Redis")
+
+    lb >> workers
+    workers >> fw >> dbs
+    workers >> s3
+    workers >> kfk
+    workers >> redis
+
+with Diagram("PAM Servers Pro 2XL", show=False):
     lb = ELB("lb")
 
     with Cluster("EKS"):
